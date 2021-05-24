57d99cea2db92119a032009600587f3fb3ede400
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-05-24 19:36:31 +0100

    Two endpoints, index and /user. Values are dynamic. Readme included
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-dynamic-website
 A Python Flask dynamic website - User registration, login and dynamic, unique content per user.
+
+## Running this website
+If you are running locally, make sure you have installed all Python requirements by: `pip install -f requirements.txt` then `flask run`.
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
+from datetime import datetime
+
+user_details = {"username": "ByteSize", "website": "https://bytesize.codes"}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html", user=user_details, motto="Make links shrink", site_name="ByteSize Link Shrinker", time=datetime.now())
+
+@app.route('/user')
+def user():
+    return render_template("user.html", user=user_details, motto="Make links shrink", site_name="ByteSize Link Shrinker")
