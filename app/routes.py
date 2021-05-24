37e08219646e5f433b9fbd98b3075a960ffe62b6
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-05-24 19:50:07 +0100

    Include shortened link redirect page
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,8 @@ def index():
 @app.route('/user')
 def user():
     return render_template("user.html", user=user_details, motto="Make links shrink", site_name="ByteSize Link Shrinker")
+
+@app.route('/<path:link>')
+def shortened_link(link):
+	link_details = {"href": "https://twitter.com/ByteSizeEvents", "endpoint": link}
+	return render_template("link.html", link_details=link_details, motto="Make links shrink", site_name="ByteSize Link Shrinker")
