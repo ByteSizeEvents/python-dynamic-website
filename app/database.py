cfe0867dc1b8f5ef5385f982e3984544443cf7fa
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-05-25 10:21:52 +0100

    Add endpoint to create a link and add it into the database. Redirect creations to confirmation page
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- app/database.py
+++ app/database.py
@@ -1,21 +1,47 @@
 import mysql.connector
+import uuid
+
+database = "link_shortener"
 
 def mysql_connection():
     return mysql.connector.connect(user='root', password='password123',
                                 host='127.0.0.1',
-                                database='link_shortener',
+                                database=database,
                                 auth_plugin='mysql_native_password')
 
-def fetch_link(shorturl):
+def fetch_link(shorturl="", id=""):
     connection = mysql_connection()
     cursor = connection.cursor(dictionary=True, buffered=True)
 
-    query = ("SELECT name, href FROM link_shortener.link_details WHERE shorturl = %s;")
-    cursor.execute(query, (shorturl,))
+    if shorturl:
+        query = (f"SELECT id, name, href, shorturl FROM {database}.link_details WHERE shorturl = %s;")
+        cursor.execute(query, (shorturl,))
+    else:
+        query = (f"SELECT name, href, shorturl FROM {database}.link_details WHERE id = %s;")
+        cursor.execute(query, (id,))
 
     result = cursor.fetchone()
 
     cursor.close()
     connection.close()
     return result
-    
+
+def create_link(link_name, link_href):
+    connection = mysql_connection()
+    cursor = connection.cursor()
+
+    uuid = random_uuid()
+
+    query = (f"INSERT INTO {database}.link_details (name, href, shorturl) VALUES (%s, %s, %s);")
+    cursor.execute(query, (link_name, link_href, uuid))
+    connection.commit()
+
+    entered_id = cursor.lastrowid
+    shorturl = fetch_link("", entered_id)["shorturl"]
+
+    cursor.close()
+    connection.close()
+    return shorturl
+
+def random_uuid():
+    return str(uuid.uuid4())[:5]
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, database
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from datetime import datetime
 
 user_details = {"username": "ByteSize", "website": "https://bytesize.codes"}
@@ -13,7 +13,23 @@ def index():
 def user():
     return render_template("user.html", user=user_details, motto="Make links shrink", site_name="ByteSize Link Shrinker")
 
-@app.route('/<path:link>')
+@app.route('/user/create', methods=['GET', 'POST'])
+def user_create():
+	if request.method == 'GET':
+	    return render_template("user_create.html", user=user_details, motto="Make links shrink", site_name="ByteSize Link Shrinker")
+	elif request.method == 'POST':
+		link_name = request.form["link_name"]
+		link_href = request.form["link_href"]
+		shorturl = database.create_link(str(link_name), str(link_href))
+		return redirect(url_for("user_create_submit", link = shorturl))
+
+@app.route('/user/created/<string:link>', methods=['GET'])
+def user_create_submit(link):
+	print(f"Link requested is '{link}'")
+	link_details = database.fetch_link(str(link))
+	return render_template("user_created.html", link_details=link_details, user=user_details, motto="Make links shrink", site_name="ByteSize Link Shrinker")
+
+@app.route('/<string:link>')
 def shortened_link(link):
 	print(f"Link requested is '{link}'")
 	link_details = database.fetch_link(str(link))
